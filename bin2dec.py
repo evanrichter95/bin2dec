eb38560667b4c37942e3bdb62ab0953217dfc551
evanrichter95 <evanrichter@Evans-MacBook-Air.local>
2014-09-09 10:45:58 -0400

    first commit

--- bin2dec.py
+++ bin2dec.py
@@ -1,4 +1,9 @@
-# this program converts binary numbers to decimal numbers 
-
-def dec2bin():
-  pass
+ def bin2dec(num):
+  # takes a binary numver as in input and returns the pinary equivalent
+  # num is a string representing a binary number like "10101"
+  result = 0
+  for index in range(len(num)):
+    digit = num[index]
+    if digit == "1":
+      result = result + pow(2,len(num)-index-1)
+    return result 
